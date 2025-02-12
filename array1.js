623cef809fb19818ce553cd80695bdd650e64ab6
satya191sai <sai67038@gmail.com>
2025-02-12 10:24:25 +0530

    sai

---
+++ array1.js
@@ -0,0 +1,15 @@
+let eids=[101,102,103,104];
+console.log(eids[0]);
+console.log(eids[14]);
+name =["satya","kumar","roopa"]
+table =[10,100,30,40,"vinay","tarun"]
+console.log(name);
+console.log(table);
+console.log(eids);
+for(i=0;i<=3;i++){
+console.log(eids[i]);
+}
+for(let eid of name)
+{
+    console.log(eid);
+}
