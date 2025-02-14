d9cc2d6cffc7951de6552333e81c57821a60eb4f
satya191sai <sai67038@gmail.com>
2025-02-14 09:48:38 +0530

    hgjh

--- genderusingwhile.js
+++ genderusingwhile.js
@@ -9,12 +9,22 @@ let emp=[{fname:"John", lname:"Doe", age:25,gender:"male"},
     {fname:"roopa", lname:"satya", age:40,gender:"male"},
     {fname:"roopa", lname:"satya", age:40,gender:"female"},
     {fname:"sai", lname:"kumar", age:30,gender:"female"},
-    {fname:"roopa", lname:"satya", age:40,gender:"male"}
+    {fname:"roopa", lname:"satya", age:40,gender:"bigender"}
 ]
+let no_Of_Female_Users=0;
+let no_Of_Female_Userss=0;
 let i=0;
-while(emp.gender==="female")
-    {
+while(i<=emp.length-1){
+    if(emp[i].gender ==="female"){
+        no_Of_Female_Users++;
+    }
     i++;
 }
-console.log(i)
-console.log(emp.length)
+console.log(no_Of_Female_Users)
+
+for(let j=0;j<=emp.length-1;j++){
+    if(emp[j].gender ==="male"){
+        no_Of_Female_Users++;
+    }
+}
+console.log(no_Of_Female_Userss)
