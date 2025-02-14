bef050959cad31d0998ab1656f7f66d19d145342
satya191sai <sai67038@gmail.com>
2025-02-14 10:18:58 +0530

    hgjh

---
+++ checkingobjempty.js
@@ -0,0 +1,13 @@
+let emps={};
+    
+    let object_Length = Object.entries(emps).length;
+    
+if (object_Length<=0){
+    console.log("object is empty")
+}else{
+    console.log("object is not empty")
+    console.log("the lenth of object is",object_Length);
+}
+
+
+    
