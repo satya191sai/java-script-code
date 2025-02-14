5d2e8688e3ac036cbad66f4a8667b54d723d2714
satya191sai <sai67038@gmail.com>
2025-02-14 10:04:21 +0530

    hgjh

---
+++ forin.js
@@ -0,0 +1,5 @@
+let details={fname:"John", lname:"Doe", age:25,gender:"male"};
+for(let key in details){
+    console.log(details[key])
+
+}
---
+++ verifyemptyarray.js
@@ -0,0 +1,11 @@
+let emps=[101];
+lenth_of_array=emps.length;
+if (lenth_of_array<=0)
+{
+    console.log("array is emply")
+}
+else{
+    console.log("array is not empty")
+    console.log("array lenth is",lenth_of_array)
+    
+}
