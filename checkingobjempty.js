7492d5b8989a73d480b569b21aaa4a10b28ba6c6
satya191sai <sai67038@gmail.com>
2025-02-17 10:50:22 +0530

    dsfsD

--- checkingobjempty.js
+++ checkingobjempty.js
@@ -1,4 +1,4 @@
-let emps={name:"sai",id:102};
+let emps={};
     
     let object_Length = Object.entries(emps).length;
     /*object is empty using object.entries method*/
@@ -9,16 +9,15 @@ if (object_Length<=0){
     console.log("the lenth of object is",object_Length);
 }
 /* objects is empty first converting into array*/
-let x=Object.keys(emps).length
-console.log(x)
-if(x.length>0){
-    console.log("object is empty")   
+let x=Object.values(emps).length
+if(Object.keys(emps).length<=0){
+    console.log("object is empty")  
 }
 else{
     console.log("object is not empty")
     console.log("the lenth of object is",x);
 }
 /*object empty  using ternary operator*/
-    x.length>0?
+    x<=0?
     console.log("object is empty"):
     console.log("object is not empty")
