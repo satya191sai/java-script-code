7aa520f342e58ac64e3f7bf1e585ccb5efb2f437
satya191sai <sai67038@gmail.com>
2025-02-18 11:58:00 +0530

    hgh

---
+++ function.js
@@ -0,0 +1,38 @@
+/*function add(a,b)
+{
+  console.log(a+b);
+  console.log(a*b);
+  
+}
+add(1,2)
+add(1,"sai")
+add(1)
+add(1,2,3)*/
+
+/*function add(a,b){
+   let c=1
+    
+   return a+b+c   
+}
+add(1,2,)
+t=add(1,2,3)
+console.log(t);*/
+
+
+ function cal_age(year){   
+ let current=new Date().getFullYear();
+ let current1=new Date().getMonth();
+ let current2=new Date().getDate();
+console.log(current);
+console.log(current1);
+console.log(current2);
+let ob=new Date();
+console.log(ob.getTime());
+return current-year
+ }
+ let age=cal_age(1997);
+ console.log(age);
+
+ 
+
+
