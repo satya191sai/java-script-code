f57f445530a95f07bc3fb270e60c6f4135de6a9c
satya191sai <sai67038@gmail.com>
2025-03-18 12:04:59 +0530

    DSFASD

---
+++ arrays/arrays.js
@@ -0,0 +1,6 @@
+let employees=[101,102,103,104,105]
+for (let i=0;i<=employees.length-1;i++)
+{
+    console.log(employees[i]);
+    
+}
---
+++ controlstatemens/forloop.js
@@ -0,0 +1,12 @@
+// for (let i=1;i<=5;i++){
+//     console.log(i);
+// }
+
+// for (let i=1;i<=10;i++){
+//     if (i%2===0){
+//         console.log(i);   
+//     }
+// }
+for (let i=1;i<=10;i=i+2){
+            console.log(i);   
+        }
