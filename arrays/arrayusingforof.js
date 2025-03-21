ca0cc2c327c7da6b7e65e248ff0febe557444be6
satya1919sai <sai67038@gmail.com>
2025-03-21 11:11:45 +0530

    sfdf

--- arrays/arrayusingforof.js
+++ arrays/arrayusingforof.js
@@ -1,7 +1,3 @@
-let employees=[101,102,103,104,105]
-let i=0
-for (let emp of employees)
-{
-    console.log(emp);
-    i++
-}
+let  a=20;
+console.log(a);
+
--- objects/sampleobj.js
+++ objects/sampleobj.js
@@ -1,12 +1,15 @@
-let emp=[{
-    ename:"satya",
-    ename:"kumar",
-    id:56,
-    sal:78990
-},{bname:"satya",
-    id:87,
-    sal:7687
-}]
-for(let em in emp){
-console.log(emp[em]);
-}
+let employees={ename:"satya",id:56,sal:78990
+    ,bname:"sonia",id1:87,sal2:7687
+    ,cname:"kumar",id2:78,sal1:878978}
+// for(let i=0;i<=emp.length-1;i++){ 
+//    console.log(emp[i].ename);
+// }
+// let i=0
+// while(i<=emp.length-1){
+//     console.log(emp[i].id,emp[i].ename);
+//     i++
+// }
+for(let emp in employees){
+    console.log(emp,":",employees[emp]);
+    }
+    
